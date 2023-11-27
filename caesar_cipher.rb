f6f2571900fb1e905eec91676c5dedab642fde3d
raliwalas <rachel.aliwalas52@gmail.com>
2023-11-27 15:14:53 -0500

    brainstorming solution

---
+++ caesar_cipher.rb
@@ -0,0 +1,35 @@
+
+
+def caesar_cipher(phrase, shift_by)
+
+    alphabet = ("a"..."z").to_a
+
+    cipher = ""
+
+    phrase.split.to_a!
+
+    
+
+    
+    
+=begin
+- returns a string back
+- make sure the capitalization stays the same
+- make sure punctuation stays the same
+- maybe want to split the string phrase and save in an array?
+    - phrase.split
+
+- but have to make sure that z goes back to a
+
+
+
+=end
+    
+
+
+end
+
+=begin
+caesar_cipher("What a string!", 5)
+=> "Bmfy f xywnsl!"
+=end
