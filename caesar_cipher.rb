3634a3abb79eec62f7a37a2a5a6e3c32458b7d8c
raliwalas <rachel.aliwalas52@gmail.com>
2023-11-27 21:21:22 -0500

    reworking plan with pseudocode

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,26 +1,30 @@
 
+# also using replit.com and irb to run things
 
 def caesar_cipher(phrase, shift_by)
 
-    alphabet = ("a"..."z").to_a
+    alphabet = ("a".."z").to_a
 
-    cipher = ""
-
-    phrase.split.to_a!
-
-    
-
-    
+    return phrase    
     
 =begin
 - returns a string back
 - make sure the capitalization stays the same
 - make sure punctuation stays the same
-- maybe want to split the string phrase and save in an array?
-    - phrase.split
 
-- but have to make sure that z goes back to a
+phrase.each do |letter|
 
+1. find desired letter in the alphabet array
+    - original_position = alphabet.find_index(i)
+
+2. find the new coded letter
+    - new_position = original_position + shift_by is the new letter
+
+3. if the new position is > 25
+     new_position - 26 should loop it back correctly
+
+4. replace the letter
+    phrase[original_position] = alphabet[new_position]
 
 
 =end
