92d52cbce6f25715b6cf4689332a07b14d3b7436
raliwalas <rachel.aliwalas52@gmail.com>
2023-12-04 12:13:16 -0500

    works for long phrases - project complete!

--- sub_strings/sub_strings.rb
+++ sub_strings/sub_strings.rb
@@ -52,3 +52,6 @@ dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","o
 
 substrings("Below", dictionary)
 #  => {"below" => 1, "low" => 1 }
+
+substrings("Howdy partner, sit down! How's it going?", dictionary)
+#  => { "down" => 1, "go" => 1, "going" => 1, "how" => 2, "howdy" => 1, "it" => 2, "i" => 3, "own" => 1, "part" => 1, "partner" => 1, "sit" => 1 }
