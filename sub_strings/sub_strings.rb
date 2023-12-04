942aae8a87c44686cea6e2a38a15cc8d7bab9bab
raliwalas <rachel.aliwalas52@gmail.com>
2023-12-04 12:10:24 -0500

    returns substrings in hash - need more testing with longer phrases

--- sub_strings/sub_strings.rb
+++ sub_strings/sub_strings.rb
@@ -1,11 +1,9 @@
 
 def substrings(word, dictionary)
 
-    #not counted
-    subs = Array.new
   
-    # will be final returned hash
-    substrings_counted = Hash.new
+    subs = Hash.new(0)
+    
     
     # isolating each letter of the string
     word_arr = word.downcase!.split("").to_a
@@ -19,73 +17,36 @@ def substrings(word, dictionary)
   
         if dictionary.include?(current_sub) == true
           
-          subs.push(current_sub)
+          subs[current_sub] += 1
         end
       end
   
-      puts "substrings: #{subs}"
     end
   
-   
+    return subs
   
+
+  
+ 
+
 =begin
-  
-  
+
+
 ----------------------- from basic enumerable methods section
 
 votes = ["Bob's Dirty Burger Shack", "St. Mark's Bistro", "Bob's Dirty Burger Shack"]
 
 votes.reduce(Hash.new(0)) do |result, vote|
-result[vote] += 1
-result
+  result[vote] += 1
+  result
 end
 #=> {"Bob's Dirty Burger Shack"=>2, "St. Mark's Bistro"=>1}
 
---------------------------
-
-word.each_index do |start|    
-
-    word.each_index do |finish|
-
-        if dictionary.include?(word[start..finish]) == true
-
-            puts word[start..finish]
-
-        else
-            puts "nope"
-        end
-    end
-end
-
----- pseudocode ----
-
-    word.each_index do |i|
-    - dictionary.include?(word[i])
-    - see if there are any 1 letter strings
-
-
-    same thing but with ranges
-        - maybe need a loop within a loop to iterate through all the letters?
-            words.all then words.any ???
-
-        - word[0..1], word[0..2], word[0..3], until get to full word 
-            - word.length()
-        - word[1..2], word[1..3], word[1..4] until word.length()
-        - word[2..3], word[2..4] etc etc
-
-    if dictionary.include(word[whatever range]) == true
-        - are there other methods instead of include? that would work?
-
-        - save that substring (word[whatever range]) as a key
-        - value is the counter -- make sure that it doesn't make a new key if already exists
-
-
-    - returns a hash
-  
 =end
-  
+
+
 end
-  
+
 
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
