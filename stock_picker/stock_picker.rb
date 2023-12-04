f2aef6092f36da4b2cc294d55b8b28fa4e18a13b
raliwalas <rachel.aliwalas52@gmail.com>
2023-12-04 16:17:42 -0500

    reworking brainstorming

--- stock_picker/stock_picker.rb
+++ stock_picker/stock_picker.rb
@@ -3,7 +3,30 @@ def stock_picker(stocks)
 
     
 
-    #return array, containing 2 days for buying and selling for best profit
-
-
 end
+
+
+=begin
+-------brainstorming-----
+
+- buy first, sell second number
+- do opposite of what I said before
+
+    sell value - buy value
+
+    - sell value can NOT be stock[0]
+    - buy value can NOT be the stock[-1]
+
+- needs to go through all of the stock price options
+
+- need logic to find which pair has the highest profit? compare profits
+
+- try to be more effient with the code this time!
+
+=end
+
+
+
+
+stock_picker([17,3,6,9,15,8,6,1,10])
+#=> [1,4]  # for a profit of $15 - $3 == $12
