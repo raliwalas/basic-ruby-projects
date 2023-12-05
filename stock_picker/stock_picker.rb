ec9d0b5d76db96995a5b86a0bf70a7e189a3520e
raliwalas <rachel.aliwalas52@gmail.com>
2023-12-05 13:24:21 -0500

    finds solution if input changes...fixme!

--- stock_picker/stock_picker.rb
+++ stock_picker/stock_picker.rb
@@ -30,8 +30,8 @@ def stock_picker(stock_prices)
     
   end
   
-  stock_picker([17,3,6,9,15,8,6,1,10])
-  #=> [1,4]  # for a profit of $15 - $3 == $12
+stock_picker([17,3,6,9,15,8,6,1,10, 1])
+#=> [1,4]  # for a profit of $15 - $3 == $12
 
 
 stock_picker([17,3,6,9,15,8,6,1,10])
