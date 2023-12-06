5bfb4442aae104ec6c35662f07871a7d44c00ad4
raliwalas <rachel.aliwalas52@gmail.com>
2023-12-06 16:47:55 -0500

    Created is_sorted? method for bubble_sort
    Added the is_sorted? method so that later, I can just start the loop and
    keep it going until is_sorted? returns true. This allows for multiple
    cycles of the bubble sort when that is finished later.

--- bubble_sort/bubble_sort.rb
+++ bubble_sort/bubble_sort.rb
@@ -1,8 +1,48 @@
-def bubble_sort(array)
+def bubble_sort(arr)
 
-    
+# do the bubble sort unless is_sorted? == true
+
+# array[0], array[3] = array[3], array[0]
+# from https://coderwall.com/p/be_1va/swap-two-elements-of-an-array-in-ruby 
 
 end
 
-bubble_sort([4,3,78,2,0,2])
+
+=begin
+
+is_sorted? checks whether is array is sorted or now and returns a boolean value
+
+If the last element of the array is not the maximum within the array, we automatically know that
+the array is not sorted. If it is, then remove that value and check that the new last value is the maximum. 
+
+If we end up with an array of length 1, we know the list was sorted, or the input was only length 1.
+=end
+
+def is_sorted?(arr)
+
+sorted = false
+
+arr.each_with_index do |num, index|
+
+    if arr.last == arr.max
+        arr.pop
+    end
+    
+    if arr.length == 1
+        sorted = true
+    else 
+        sorted = false
+    end 
+    
+    return sorted  
+
+end
+end
+
+#is_sorted?([1,2,3])
+#is_sorted?([5,3,4,6,10])
+#is_sorted?([4,3,78,2,0,2])
+
+#bubble_sort([1,2,3])
+#bubble_sort([4,3,78,2,0,2])
 # => [0,2,2,3,4,78]
