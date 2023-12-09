ebe006e8d1520fbe0341604559642d1aba74f81a
raliwalas <rachel.aliwalas52@gmail.com>
2023-12-08 21:21:44 -0500

    Progress on Bubble Sort - FIXME ArgumentError
    It is close, but breaks because of some indexing. The for loop inside the until loop needs to be fixed becasue it does not iterate through the array properly.
    After rewatching the Harvard CS50 lesson again, I realized that my
    earlier approach was for a different algorithm (was not committed here
    as I was working in replit). I need to find the
    largest number in the array, sort that to the end of the array, solidify
    that position, then find the next largest number in array (0..n-1) then
    repeat (0..n-2). At first, I was trying to follow the video and use k as well,
    but I had difficulty - I am not sure if I need to incorporate with how I
    have things set up.

--- bubble_sort/bubble_sort.rb
+++ bubble_sort/bubble_sort.rb
@@ -1,48 +1,96 @@
 def bubble_sort(arr)
 
-# do the bubble sort unless is_sorted? == true
-
-# array[0], array[3] = array[3], array[0]
-# from https://coderwall.com/p/be_1va/swap-two-elements-of-an-array-in-ruby 
-
-end
-
-
-=begin
-
-is_sorted? checks whether is array is sorted or now and returns a boolean value
-
-If the last element of the array is not the maximum within the array, we automatically know that
-the array is not sorted. If it is, then remove that value and check that the new last value is the maximum. 
-
-If we end up with an array of length 1, we know the list was sorted, or the input was only length 1.
-=end
-
-def is_sorted?(arr)
-
-sorted = false
-
-arr.each_with_index do |num, index|
-
-    if arr.last == arr.max
-        arr.pop
+    answer = is_sorted?(arr)
+    
+  
+    #puts arr.inspect 
+  
+    # puts "answer: #{answer}"
+  
+    # puts "====="
+    # puts arr.inspect
+  
+    #doing -1 because i want to go from 0..n in for loop
+    #k = 1
+  
+    until answer == true do 
+  
+      n = arr.length - 1
+  
+      for i in 0..n do
+  
+        puts i
+        puts "-----"
+  
+        max = arr[0..n].max
+        index = arr.find_index(max).to_i
+        neighbor = index + 1
+  
+        if max > arr[neighbor].to_i
+          arr[index], arr[neighbor] = arr[neighbor], arr[index] 
+        end
+  
+        puts arr.inspect
+        puts n
+  
+        n -= 1
+  
+        #answer = is_sorted?(arr)
+        
+      end
+  
+      #k +=1
+  
+      answer = is_sorted?(arr)
+  
     end
+  
+    puts " HELLOOOO "
+    puts arr.inspect
     
-    if arr.length == 1
-        sorted = true
-    else 
-        sorted = false
-    end 
-    
-    return sorted  
-
 end
+  
+  
+=begin
+  
+  is_sorted? checks whether an is array is sorted or not and returns a boolean value
+  
+  If the last element of the array is not the maximum within the array, we automatically know that
+  the array is not sorted. If it is, then remove that value and check that the new last value is the maximum. 
+  
+  If we end up with an array of length 1, we know the list was sorted, or the input was only length 1.
+=end
+  
+def is_sorted?(arr)
+  
+    copy = []
+    
+    # using clone because copy = arr will cause changes to the original arr
+    copy = arr.clone
+  
+    sorted = false
+  
+    copy.each_with_index do |num, index|
+  
+      if copy.last == copy.max
+        copy.pop
+      end
+      
+      if copy.length == 1
+        sorted = true
+      else 
+        sorted = false
+      end 
+  
+    end
+  
+    return sorted  
 end
 
 #is_sorted?([1,2,3])
 #is_sorted?([5,3,4,6,10])
 #is_sorted?([4,3,78,2,0,2])
 
-#bubble_sort([1,2,3])
-#bubble_sort([4,3,78,2,0,2])
+#bubble_sort([3,4,1])
+bubble_sort([4,3,78,2,0,2])
 # => [0,2,2,3,4,78]
