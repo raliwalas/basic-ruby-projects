a3002a9550868d84c0de26f6183aa00301bdaf33
raliwalas <rachel.aliwalas52@gmail.com>
2023-12-09 21:32:21 -0500

    FIXME - iterating through multiple times
    I can either try and fix is_sorted? so that it can handle duplicate
    values or put the for loop in another loop that checks with something
    else (maybe if there have been no swaps?)
    
    --- ideas ---
    - use uniq!
    	- worry about the order of things since it only keeps first
    	  value that appears
    - use count
    	- would have to come up with some if/else statements to handle
    	- if count > 1, find the indexes. if they are the current max
    	  value and check indexes are the last values of the array
    - a.detect{ |e| a.count(e) > 1 }
    	- from https://stackoverflow.com/questions/8921999/how-to-find-and-return-a-duplicate-value-in-array

--- bubble_sort/bubble_sort.rb
+++ bubble_sort/bubble_sort.rb
@@ -1,86 +1,52 @@
+def bubble_sort(arr)
 
 
-=begin
-----brainstorming-----
-
-n = arr.length
-
-for k in 1..n do
-
-find the biggest element
-find the index of the biggest element
-
-if the max index != n
-    swap with the neighbor
+    #doing -1 because i want to go from 0..n in for loop
+    #k = 1
+  
+  
+    # n is the number of elements in the array
+    # -1 to go from 0 to last element, not 1 to n -1
+    n = arr.length - 1
+  
+    # k is the number of iterations 
+    for k in 0..n do
+  
+      max = arr[0..n].max
+      max_index = arr.find_index(max).to_i
+  
+      puts "n #{n}"
+      puts "k #{k}"
+      puts "max: #{max}"
+      puts "max_index: #{max_index}"
+      puts 
+      puts arr.inspect
+  
+      if max_index != n
+        neighbor = max_index + 1
+        # if the largest number is not at the end of the array, swap it with it's neighbor
+        arr[max_index], arr[neighbor] = arr[neighbor], arr[max_index]
+      #else 
+        #n-=1
+      end
+  
+      if max_index == n
+        n -= 1
+        puts "here"
+      end
+  
+  
+      puts "----"
+  
+    end
+  
+  
+    puts " HELLOOOO "
+    puts arr.inspect
+  
 end
-
-then, decrease n 
-
-n-=1 
-
-i dont think i need to make it n - k 
-
-
-
-=end
-
-# def bubble_sort(arr)
-
-#   answer = is_sorted?(arr)
-
-#   #doing -1 because i want to go from 0..n in for loop
-#   #k = 1
-
-#   until answer == true do 
-
-#     # n is the number of elements in the array
-#     # -1 to go from 0 to last element, not 1 to n -1
-#     n = arr.length - 1
-
-#     # k is the number of iterations 
-#     for k in 0..n do
-
-#       max = arr[0..n].max
-#       max_index = arr.find_index(max).to_i
-
-#       puts "n #{n}"
-#       puts "k #{k}"
-#       puts "max: #{max}"
-#       puts "max_index: #{max_index}"
-#       puts 
-#       puts arr.inspect
-    
-#       if max_index != n
-#         neighbor = max_index + 1
-#         # if the largest number is not at the end of the array, swap it with it's neighbor
-#         arr[max_index], arr[neighbor] = arr[neighbor], arr[max_index]
-#       #else 
-#         #n-=1
-#       end
-
-#       if max_index == n
-#         n -= 1
-#         puts "here"
-#       end
-
-#       if is_sorted?(arr) == true
-#         break
-#       end
-
-#       puts "----"
-    
-#     end
-
-#     answer = is_sorted?(arr)
-
-#   end
-
-#   puts " HELLOOOO "
-#   puts arr.inspect
-
-# end
-
-
+  
+  
 =begin
 
 is_sorted? checks whether an is array is sorted or not and returns a boolean value
@@ -91,44 +57,44 @@ the array is not sorted. If it is, then remove that value and check that the new
 If we end up with an array of length 1, we know the list was sorted, or the input was only length 1.
 =end
 
-def is_sorted?(arr)
+# def is_sorted?(arr)
 
-copy = []
+#   copy = []
 
-# using clone because copy = arr will cause changes to the original arr
-copy = arr.clone
+#   # using clone because copy = arr will cause changes to the original arr
+#   copy = arr.clone
 
-sorted = false
+#   sorted = false
 
-copy.each_with_index do |num, index|
+#   copy.each_with_index do |num, index|
 
-    #dups = arr.count(num)
+#     #dups = arr.count(num)
 
-    #if 
+#     #if 
 
 
-    if copy.last == copy.max
-        copy.delete(copy.max)
-        puts copy.inspect
+#     if copy.last == copy.max
+#       copy.delete(copy.max)
+#       puts copy.inspect
 
-    end
-    
-    if copy.length == 1
-        sorted = true
-    else 
-        sorted = false
-    end 
+#     end
 
-end
+#     if copy.length == 1
+#       sorted = true
+#     else 
+#       sorted = false
+#     end 
 
-    puts sorted  
-end
+#   end
 
-#is_sorted?([1,2,3])
-#is_sorted?([5,3,4,6,10])
-#is_sorted?([4,3,78,2,0,2])
-is_sorted?([0,2,2,3,4,78])
+#   puts sorted  
+# end
+
+# #is_sorted?([1,2,3])
+# #is_sorted?([5,3,4,6,10])
+# #is_sorted?([4,3,78,2,0,2])
+# is_sorted?([0,2,2,3,4,78])
 
 #bubble_sort([3,4,1])
-#bubble_sort([4,3,78,2,0,2])
+bubble_sort([4,3,78,2,0,2])
 # => [0,2,2,3,4,78]
